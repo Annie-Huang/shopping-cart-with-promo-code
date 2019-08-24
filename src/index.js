2b755ddeae6c12a87307ec3a4ccdcc2daf33d187
Annie-Huang <annie.hn@gmail.com>
2019-08-24 21:31:54 +1000

    Wrapped ReduxProvider to <App> and provide store. Create configureStore.js. Create rootReducer.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+    <ReduxProvider store={store}>
+        <App />
+    </ReduxProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
