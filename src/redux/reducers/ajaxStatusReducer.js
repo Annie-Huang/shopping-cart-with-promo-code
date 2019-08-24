8f5659ec58c91a321687fa3e81cd46c652a2aa87
Annie-Huang <annie.hn@gmail.com>
2019-08-24 22:58:07 +1000

    Correct mistake in ajaxStatusReducer.js and initialState.js

--- src/redux/reducers/ajaxStatusReducer.js
+++ src/redux/reducers/ajaxStatusReducer.js
@@ -5,7 +5,7 @@ const actionTypeEndsInSuccess = (type) => {
     return type.substring(type.length - 8) === '_SUCCESS';
 };
 
-const ajaxStatusReducer = (state=initialState.ajaxCallsInProgress, action) => {
+const ajaxStatusReducer = (state=initialState.apiCallsInProgress, action) => {
     if(action.type === types.BEGIN_AJAX_CALL) {
         return state + 1;
     } else if(action.type === types.AJAX_CALL_ERROR || actionTypeEndsInSuccess(action.type)) {
--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -1,4 +1,4 @@
 export default {
-    courses: [],
+    products: [],
     apiCallsInProgress: 0
 };
