4f4eb23079fed964ede0b2dc08447f1b66c96461
Annie-Huang <annie.hn@gmail.com>
2019-08-25 21:03:42 +1000

    Add ShoppingCart.js ( discountCodes,  loadDiscountCodes) and add <ShoppingCart/> into App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 // import { ProductList } from "./components/ProductList/ProductList";
 // It will failed the build, we need the default one that got connect, not the named one use for testing.
 import ProductList from "./components/ProductList";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
     return (
         <div className="container-fluid">
             <h1>Please select your products from below...</h1>
             <ProductList />
+            <ShoppingCart/>
         </div>
     );
 }
---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from "react-redux";
+import * as discountCodeActions from "../redux/actions/discountCodeActions";
+
+export const ShoppingCart  = ({
+    discountCodes,
+    loadDiscountCodes
+}) => {
+    useEffect(() => {
+        loadDiscountCodes().catch(error => {
+            alert("Loading discount codes failed" + error);
+        });
+    }, []);
+
+    const children = discountCodes.map(discountCode =>
+        <div key={discountCode.id}>{discountCode.id} : {discountCode.description}</div>
+    );
+
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+ShoppingCart.propTypes = {
+    discountCodes: PropTypes.array.isRequired,
+    loadDiscountCodes: PropTypes.func.isRequired
+};
+
+
+const mapStateToProps = (state, ownProps) => ({
+    discountCodes: state.discountCodes,
+});
+
+const mapDispatchToProps = {
+    loadDiscountCodes: discountCodeActions.loadDiscountCodes
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ShoppingCart);
