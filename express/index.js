bcfd9eed8dd9a30812b54b759765447dcd02a0e8
Annie-Huang <annie.hn@gmail.com>
2019-09-08 15:15:19 +1000

    Create both products and discountCodes endpoints. And add logger middleware (middleware has to be added before any api calls).

--- express/index.js
+++ express/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const logger = require('./middleware/logger');
+const products = require('../src/resources/fixtures/products.json');
+const discountCodes = require('../src/resources/fixtures/discountCodes');
 
 const app = express();
 
+// Init middleware
+app.use(logger);
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    res.send('<h1>Hello World!</h1>');
     // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Gets All products
+// app.get('/api/products', (req, res) => res.json(products));
+app.get('/api/products', (req, res) => {
+    setTimeout(() => {
+        return res.json(products);
+    }, 300);
+});
+
+// Gets All discountCodes
+app.get('/api/discountCodes', (req, res) => {
+    setTimeout(() => {
+        return res.json(discountCodes);
+    }, 300);
 });
 
 const PORT = process.env.PORT || 5001;
