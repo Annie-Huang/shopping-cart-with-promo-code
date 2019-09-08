a60cf98d087eb796939ee4b84ebd5ef5ecba70b7
Annie-Huang <annie.hn@gmail.com>
2019-09-08 21:13:26 +1000

    Update unit tests for Actions now we use axios to call express backend.

--- src/redux/actions/discountCodeActions.test.js
+++ src/redux/actions/discountCodeActions.test.js
@@ -4,6 +4,9 @@ import configureMockStore from 'redux-mock-store';
 import * as types from './actionTypes';
 import * as discountCodeActions from './discountCodeActions';
 
+import axios from 'axios';
+jest.mock('axios');
+
 // Test a sync action
 describe('discountCodeActions', () => {
     it('loadDiscountCodesSuccess should create a LOAD_DISCOUNT_CODES_SUCCESS action', () => {
@@ -22,17 +25,42 @@ describe('discountCodeActions', () => {
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
 
-describe('discountCodeActions thunk', () => {
-    it('should create BEGIN_API_CALL and LOAD_DISCOUNT_CODES_SUCCESS when loading products', (done) => {
+// // The following is calling products.json file directly in mockProductApi.js
+// describe('discountCodeActions thunk', () => {
+//     it('should create BEGIN_API_CALL and LOAD_DISCOUNT_CODES_SUCCESS when loading discountCodes', (done) => {
+//
+//         // This test takes a bit longer because of the delay set in \shopping-cart-with-promo-code\src\mockApi\delay.js
+//         // But I will keep the delay because it will make running the app more real.
+//         const store = mockStore({discountCodes: []});
+//         store.dispatch(discountCodeActions.loadDiscountCodes()).then(() => {
+//             const actions = store.getActions();
+//             expect(actions[0].type).toEqual(types.BEGIN_API_CALL);
+//             expect(actions[1].type).toEqual(types.LOAD_DISCOUNT_CODES_SUCCESS);
+//             done();
+//         });
+//     });
+// });
 
-        // This test takes a bit longer because of the delay set in \shopping-cart-react\src\api\delay.js
-        // But I will keep the delay because it will make running the app more real.
-        const store = mockStore({discountCodes: []});
-        store.dispatch(discountCodeActions.loadDiscountCodes()).then(() => {
-            const actions = store.getActions();
-            expect(actions[0].type).toEqual(types.BEGIN_API_CALL);
-            expect(actions[1].type).toEqual(types.LOAD_DISCOUNT_CODES_SUCCESS);
-            done();
+// The following is calling products.json file through axios that serve through express in mockProductApi.js
+describe("Async Actions", () => {
+    describe("Load discountCodes Thunk", () => {
+        // If you have multiple thunks, you can copy/paste this pattern to test them quickly.
+        it("should create BEGIN_API_CALL and LOAD_DISCOUNT_CODES_SUCCESS when loading products", () => {
+            // This captures all axios calls and responds with some mock data.
+            const discountCodes = require('../../resources/fixtures/discountCodes');
+            const resp = {data: discountCodes};
+            axios.get.mockResolvedValue(resp);
+
+            // Goal: Assert these actions are created.
+            const expectedActions = [
+                { type: types.BEGIN_API_CALL },
+                { type: types.LOAD_DISCOUNT_CODES_SUCCESS, discountCodes }
+            ];
+
+            const store = mockStore({ discountCodes: [] });
+            return store.dispatch(discountCodeActions.loadDiscountCodes()).then(() => {
+                expect(store.getActions()).toEqual(expectedActions);
+            });
         });
     });
 });
--- src/redux/actions/productActions.test.js
+++ src/redux/actions/productActions.test.js
@@ -4,6 +4,9 @@ import configureMockStore from 'redux-mock-store';
 import * as types from './actionTypes';
 import * as productActions from './productActions';
 
+import axios from 'axios';
+jest.mock('axios');
+
 // Test a sync action
 describe('productActions', () => {
     it('loadProductsSuccess should create a LOAD_PRODUCTS_SUCCESS action', () => {
@@ -22,17 +25,42 @@ describe('productActions', () => {
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
 
-describe('productActions Thunk', () => {
-    it('should create BEGIN_API_CALL and LOAD_PRODUCTS_SUCCESS when loading products', (done) => {
+// // The following is calling products.json file directly in mockProductApi.js
+// describe('productActions Thunk', () => {
+//     it('should create BEGIN_API_CALL and LOAD_PRODUCTS_SUCCESS when loading products', (done) => {
+//
+//         // This test takes a bit longer because of the delay set in \shopping-cart-with-promo-code\src\mockApi\delay.js
+//         // But I will keep the delay because it will make running the app more real.
+//         const store = mockStore({products: []});
+//         store.dispatch(productActions.loadProducts()).then(() => {
+//             const actions = store.getActions();
+//             expect(actions[0].type).toEqual(types.BEGIN_API_CALL);
+//             expect(actions[1].type).toEqual(types.LOAD_PRODUCTS_SUCCESS);
+//             done();
+//         });
+//     });
+// });
 
-        // This test takes a bit longer because of the delay set in \shopping-cart-react\src\api\delay.js
-        // But I will keep the delay because it will make running the app more real.
-        const store = mockStore({products: []});
-        store.dispatch(productActions.loadProducts()).then(() => {
-            const actions = store.getActions();
-            expect(actions[0].type).toEqual(types.BEGIN_API_CALL);
-            expect(actions[1].type).toEqual(types.LOAD_PRODUCTS_SUCCESS);
-            done();
+// The following is calling products.json file through axios that serve through express in mockProductApi.js
+describe("Async Actions", () => {
+    describe("Load products Thunk", () => {
+        // If you have multiple thunks, you can copy/paste this pattern to test them quickly.
+        it("should create BEGIN_API_CALL and LOAD_PRODUCTS_SUCCESS when loading products", () => {
+            // This captures all axios calls and responds with some mock data.
+            const products = require('../../resources/fixtures/products.json');
+            const resp = {data: products};
+            axios.get.mockResolvedValue(resp);
+
+            // Goal: Assert these actions are created.
+            const expectedActions = [
+                { type: types.BEGIN_API_CALL },
+                { type: types.LOAD_PRODUCTS_SUCCESS, products }
+            ];
+
+            const store = mockStore({ products: [] });
+            return store.dispatch(productActions.loadProducts()).then(() => {
+                expect(store.getActions()).toEqual(expectedActions);
+            });
         });
     });
 });
